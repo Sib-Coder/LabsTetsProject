45a3bd78318c7d827a3f631918543c6094321194
Sinitsyn Daniil <80570939+Sib-Coder@users.noreply.github.com>
2023-06-07 20:34:50 +0700

    new status

---
+++ model/models.go
@@ -0,0 +1,16 @@
+package model
+
+type Registr struct {
+	Name     string
+	LastName string
+}
+
+type UserInfo struct {
+	Name      string
+	LastName  string
+	SurName   string
+	Gender    string
+	Status    string
+	DateBirth string
+	DateAdded string
+}
